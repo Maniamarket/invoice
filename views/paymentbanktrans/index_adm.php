87866e8945b4bd5b5fc23923df06c269199b1df0
ars <ars@localhost.localdomain>
2014-12-15 12:30:07 +0400

    функц. оплатa банковским переводом

--- views/paymentbanktrans/index_adm.php
+++ views/paymentbanktrans/index_adm.php
@@ -44,31 +44,39 @@ $this->params['breadcrumbs'][] = $this->title;
 	    ],
 	    'sum',
 	    [
-		'label' => 'status',
+		'attribute' => 'status',
+		'format' => 'html',
 		'value' => function($data) {
-	    return $data->status;
-	},
-	    //'value'=>function($data) { return $data->status ? 0 : 'Отправлен' ? 1 : 'Принят' ? 2 : 'Отклонен';},		
-	    //'value'=>['0'=>'Отправлен','1'=>'Принят','2'=>'Отклонен'],
+		    switch($data->status){
+			case 0: return '<span class="label label-primary">Отправлен</span>';
+			case 1: return '<span class="label label-success">Принят</span>';
+			case 2: return '<span class="label label-danger">Отклонен</span>';
+			default: return '<span class="label label-danger">Ошибка</span>';
+		    }
+		},
+	    
 	    ],
 	    [
-		'label' => 'date',
-		'value' => function($data) {
-	    return date("Y-m-d H:i:s", $data->date);
-	},
+		'attribute' => 'date',
+		'value' => function($data) {return date("Y-m-d H:i:s", $data->date);},
 	    ],
 	    [
 		'attribute' => 'buttons',
 		'format' => 'raw',
-		'value' => function ($model) {
-		    return '<a href="/paymentbanktrans/approve?id='.$model->id.'" title="Approve">'
+		'value' => function ($data) {
+		    if(empty($data->status)){ 
+		    return '<a href="/paymentbanktrans/approve?id='.$data->id.'" title="Approve">'
 			    . '<span class="glyphicon glyphicon-ok"></span>'
 			    . '</a>&nbsp;&nbsp;'
-			    . '<a href="/paymentbanktrans/cancel?id='.$model->id.'" title="Cancel">'
+			    . '<a href="/paymentbanktrans/cancel?id='.$data->id.'" title="Cancel">'
 			    . '<span class="glyphicon glyphicon-remove"></span>'
-			    . '</a>';	
-		    //return '<a class="btn btn-success" href="/ru/paymentbanktrans/create">Create Paymentbanktrans</a><a class="btn btn-" href="/ru/paymentbanktrans/create">Create Paymentbanktrans</a>';	
+			    . '</a>';
+		    } else {
+		    return  '<span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;'			    
+			    . '<span class="glyphicon glyphicon-remove"></span>';		    	
+		    }		    
 		},
+		
 	    ],
 	    ['class' => 'yii\grid\ActionColumn',],
 	],
