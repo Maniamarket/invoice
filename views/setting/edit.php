b5e831bceb31b7539d2cad280af0d114a95291f5
Дмитрий Хе <XeDmitry@yandex.ru>
2015-03-05 19:15:00 +0600

    fix design update user

--- views/setting/edit.php
+++ views/setting/edit.php
@@ -5,7 +5,11 @@ use app\models\User;
 use app\models\Setting;
 use yii\helpers\Url;
 
+$this->title = Yii::t('app', 'User Update');
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
+<h1 class="title"><?php echo $this->title; ?></h1>
 
 <div class="form">
 <?php
@@ -29,17 +33,15 @@ use yii\helpers\Url;
             <?php echo $form->field($model, 'user_id',['labelOptions'=>['class'=>'control-label col-md-4'],
                 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"]
             )->textInput(['disabled'=>'disabled']) ; ?>
+            <?php  echo $form->field($user, 'role',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])
+                ->dropDownList(User::getRoleArray(),[]); ?>
         </div>
         <div class="fieldset-column pull-right">
             <?php  echo $form->field($user, 'email',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])->textInput(); ?>
             <?php  echo $form->field($user, 'password_',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])->textInput(); ?>
+            <?php  echo $form->field($user, 'parent_id',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])
+                ->dropDownList(User::getParentArray(),[]); ?>
         </div>
-    </div>
-    <div class="fieldset-column pull-right">
-        <?php  echo $form->field($user, 'role',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])
-            ->dropDownList(User::getRoleArray(),[]); ?>
-        <?php  echo $form->field($user, 'parent_id',['labelOptions'=>['class'=>'control-label col-md-4'], 'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>"])
-            ->dropDownList(User::getParentArray(),[]); ?>
     </div>
 
     <div class="form-group">
@@ -47,16 +49,12 @@ use yii\helpers\Url;
     </div>
     <div class="row">
         <div class="fieldset-column pull-left">
-            <?php echo Html::label('Income - ','', []); ?>
-            <?php echo Html::label($model->surtax,'', ['class'=>'control-label']) ?>
-            <?php echo Html::label('Default template - ','', []); ?>
-            <?php echo Html::label(Setting::List_Templates()[$model->def_template],'', ['class'=>'control-label']) ?>
+            <?php echo $form->field($model, 'surtax',['labelOptions'=>['class'=>'control-label']])->textInput(['disabled'=>'disabled']) ; ?>
+            <?php echo $form->field($model, 'def_template',['labelOptions'=>['class'=>'control-label']])->dropDownList(Setting::List_Templates(['prompt'=>'-Choose a Template-']),['disabled'=>'disabled']); ?>
         </div>
         <div class="fieldset-column pull-right">
-            <?php echo Html::label('Default Vat - ','', []); ?>
-            <?php echo Html::label(Setting::List_Vat()[$model->def_vat_id],'', ['class'=>'control-label']) ?>
-            <?php echo Html::label('Default company - ','', []); ?>
-            <?php echo Html::label(Setting::List_company()[$model->def_company_id],'', []) ?>
+            <?php echo $form->field($model, 'def_vat_id',['labelOptions'=>['class'=>'control-label']])->dropDownList(Setting::List_vat(['prompt'=>'-Choose a Vat-']), ['disabled'=>'disabled']); ?>
+            <?php echo $form->field($model, 'def_company_id',['labelOptions'=>['class'=>'control-label']])->dropDownList(Setting::List_company(['prompt'=>'-Choose a Company-']), ['disabled'=>'disabled']); ?>
         </div>
     </div>
 
@@ -70,7 +68,9 @@ use yii\helpers\Url;
 
     <div class="row">
         <div class="fieldset-column pull-left">
-            <?php echo Html::label($model->credit,'', [])?>
+            <?php echo $form->field($model, 'credit',['labelOptions'=>['class'=>'control-label']])->textInput(['disabled'=>'disabled']) ; ?>
+            <a href="<?= Url::toRoute(['user/add_credit','id'=>$user->id]) ?>"  class="btn btn-action" >
+                <?php echo Yii::t('app','Add Credits'); ?></a>
         </div>
     </div>
 <?php  ActiveForm::end(); ?>
