b00801d5b3cf52c301dbdd18c1d67c327a214a82
Дмитрий Хе <XeDmitry@yandex.ru>
2015-03-07 13:36:58 +0600

    fix design credits

--- views/user/payment_credit.php
+++ views/user/payment_credit.php
@@ -4,24 +4,56 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 use app\controllers\PayController;
+use app\models\Setting;
 
 
-$this->title = Yii::$app->name . ' - Payment credit';
+$this->title = Yii::$app->name . ' - ' . Yii::t('app', 'Buy credits');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<h1><?php  echo 'Payment credit';  ?></h1>
+<h1 class="title"><?php  echo Yii::t('app', 'Buy credits');  ?></h1>
 <?php $form = ActiveForm::begin([
 	'id'=>'invoice-form',
 	'enableAjaxValidation'=>false,
-	'options'=>['enctype'=>'multipart/form-data', 'role'=>'form'],
+	'options'=>['enctype'=>'multipart/form-data','class' => 'form-horizontal', 'role'=>'form'],
+    'fieldConfig' => [
+        'template' => "{label}\n<div class=\"col-md-7\">{input}</div>\n<div class=\"col-md-offset-2 col-md-7\">{error}</div>",
+    ],
 ]); ?>
-    <div class="form-group">
-          <?php echo $form->field($model, 'credit')->textInput()->hint('You credit ')->label('count credit, EUR') ; ?>
-          <?php echo Html::error($model, 'credit'); ?>
-    </div>
+<div class="row">
+    <div class="fieldset-column pull-left">
+        <?php echo $form->field($model, 'credit',['labelOptions'=>['class'=>'control-label col-md-4']])->textInput()->hint('You credit ')->label('Cost (€):') ; ?>
+        <div class="form-group field-user_payment-credit required">
+            <label class="control-label col-md-4" for="user_payment-credit">Number of Credits:</label>
+            <div class="col-md-7">
+                <?php echo Html::textInput('number_credits', $model->credit, ['class'=>'form-control', 'disabled' => 'disabled']); ?>
+            </div>
+        </div>
+        <div class="form-group field-user_payment-credit required">
+            <label class="control-label col-md-4" for="user_payment-credit">Payment:</label>
+            <div class="col-md-7">
+                <?php echo Html::dropDownList('payment_credits','',Setting::List_payment(),['id'=>'payment_credits', 'class'=>'form-control']); ?>
+            </div>
+        </div>
+        <div class="form-group field-user_payment-credit required">
+            <label class="control-label col-md-4" for="user_payment-credit">Note:</label>
+            <div class="col-md-7">
+                According to the pricing policy paypal, all buyers incur costs  worth 3.5% of the purchase amount
+            </div>
+        </div>
+        <div class="form-group field-user_payment-credit required">
+            <label class="control-label col-md-4" for="user_payment-credit">Final Cost (€):</label>
+            <div class="col-md-7">
+                <?php echo Html::textInput('number_credits', $model->credit, ['class'=>'form-control', 'disabled' => 'disabled']); ?>
+            </div>
+        </div>
+        <?php //echo '<div class="form-group"><label class="control-label col-md-5" >Payment</label><div class="col-md-6">'.Html::dropDownList('payment_credits','',Setting::List_payment(),['id'=>'payment_credits', 'class'=>'form-control']).'</div></div>'; ?>
 
-<?= Html::submitButton('Send', ['class' => 'btn btn-primary', 'name' => 'send']) ?>
+    </div>
+</div>
+<div class="form-group">
+    <?= Html::submitButton('Buy Credits', ['class' => 'btn btn-action', 'name' => 'send']) ?>
+</div>
 <?php ActiveForm::end(); ?>
 
